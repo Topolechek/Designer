dda819c560f571fa681df28c4bcb442758f4fcfa
Topolechek <48097432+Topolechek@users.noreply.github.com>
2022-10-30 12:07:12 +0300

    Update telegramm.py

--- tele/telegramm.py
+++ tele/telegramm.py
@@ -1,13 +1,13 @@
 
 import telebot
 
-<<<<<<< HEAD
-token = '5234349306:AAEhMvHbT56dSFRJlBjVL_WQvvdjasnOhA8'
-my_id = 443232407
-=======
+
+token = ''
+my_id = 4432324
+
 token = ''
 my_id = 
->>>>>>> 1b4f1a5237ffab42bd3753dbfbbdcb9639dad022
+
 bot = telebot.TeleBot(token)
 
 def send_message(text):
